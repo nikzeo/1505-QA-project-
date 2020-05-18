c2ae8881a796ac33bb1a589da4fd7ca898219fb4
nikzeo <64597157+nikzeo@users.noreply.github.com>
2020-05-18 12:12:56 +0100

    updated zeogear program

---
+++ zeogearnew/src/main/java/connect/App.java
@@ -0,0 +1,100 @@
+package connect;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class App {
+
+	private static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) throws SQLException {
+		
+		CustomerDB db = new CustomerDB();
+//			DB db2 = new DB();
+
+		try {
+			String action = "";
+			action = getAction();
+
+			do {
+
+				switch (action) {
+
+				case "create": // this works!!!!!
+
+					System.out.println("Enter CID:");
+					int customer_id = scan.nextInt();
+					scan.nextLine();
+
+					System.out.println("Enter first name: ");
+					String firstname = scan.nextLine();
+
+					System.out.println("Enter last name: ");
+					String lastname = scan.nextLine();
+
+					System.out.println("Enter email: ");
+					String email = scan.nextLine();
+
+
+					db.create(customer_id, firstname, lastname, email);;
+					break;
+
+				case "print":
+					db.readCustomer();
+					break;
+
+				case "delete":
+					System.out.println("Enter the customer_id:");
+					int customer_id1 = Integer.parseInt(scan.nextLine());
+					db.deleteCustomer(customer_id1);
+					break;
+					
+				case "update":
+					
+					System.out.println("Enter ID ");
+					int customer_id2 = scan.nextInt();
+					scan.nextLine();
+					
+					System.out.println("Enter Updated First Name ");
+					String firstname1 = scan.nextLine();
+					
+					System.out.println("Enter Updated Last Name ");
+					String lastname1 = scan.nextLine();
+					
+					System.out.println("Enter Updated Email ");
+					String email1 = scan.nextLine();
+							
+					db.updateCustomer(customer_id2, firstname1, lastname1, email1);	
+					System.out.println("Update Succesful!");
+					
+
+//				default:
+//					System.out.println("No matching case");
+
+				}
+
+				action = getAction();
+
+			} while (!action.equals("quit"));
+			System.out.println("bye");
+
+		} finally {
+
+			scan.close();
+			db.close();
+			
+		}
+		
+	}
+
+	private static String getAction2() {
+		System.out.println("Type 'product' to create a new product:"  );
+		return scan.nextLine();
+	}
+
+	private static String getAction() {
+		System.out.println("Type 'create' to create, 'print' to print, 'delete' to delete, 'update' to update or 'quit' to quit.");
+		return scan.nextLine().toLowerCase();
+	}
+
+}
---
+++ zeogearnew/src/main/java/connect/CustomerDB.java
@@ -0,0 +1,57 @@
+package connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CustomerDB {
+
+	private Connection conn;
+	private Statement stmt;
+
+	public CustomerDB() throws SQLException {
+		conn = DriverManager.getConnection(ClassConfig.DB_URL, ClassConfig.USER, ClassConfig.PASS);
+		stmt = conn.createStatement();
+	}
+
+	public void close() throws SQLException {
+		conn.close();
+	}
+
+//	public void createCustomer(String fName, String lName) throws SQLException {
+//		stmt.executeUpdate(
+//				"INSERT INTO customer (`first_name`, `last_name`)" + " VALUES ('" + fName + "', '" + lName + "')");
+//	}
+
+	public void create
+	
+	(int customer_id, String firstname, String lastname, String email) throws SQLException {
+		this.stmt = conn.createStatement();
+		String sql = "INSERT INTO customers (customer_id, firstname, lastname, email) VALUES ("+ customer_id + ", \"" + firstname + "\", \"" + lastname + "\", \""+ email + "\")";
+		stmt.executeUpdate(sql);
+	}
+
+	public void readCustomer() throws SQLException {
+		ResultSet rs = stmt.executeQuery("SELECT * FROM customers");
+		while (rs.next()) {
+			String name = rs.getString("firstname") + " " + rs.getString("lastname") + " " + rs.getString("email");
+			System.out.println(name);
+		}
+	}
+
+	public void updateCustomer(int customer_id2, String firstname1, String lastname1, String email1) throws SQLException {
+		
+		String upt = "UPDATE customers SET firstname = '" + firstname1 + "', lastname = '" + lastname1 + "', email = '" + email1 + "'WHERE customer_id = '" + customer_id2 + "'";    
+		stmt.executeUpdate(upt); 
+
+	}
+			
+	public void deleteCustomer(int customer_id) throws SQLException {
+		String delete = "DELETE FROM customers WHERE customer_id = " + customer_id + "";
+		stmt.executeUpdate(delete);
+	}}
+
+	
+	
